14e7bf2590057ae55d484eb50837ba14c41ec463
Zaky <zakyabdurrahman10@gmail.com>
2021-12-23 07:13:53 +0700

    implemented drawing pad

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
 const pad = document.querySelector('.sketch-pad');
+let mousehold;
 
-intializePad();
+intializePad(16);
+isMouseDown();
 
-function intializePad(){
-    pad.style.cssText = 'grid-template-columns: repeat(16, 1fr)';
-    totalGrid = 16 * 16;
+function intializePad(sideGrids){
+    pad.style.cssText = `grid-template-columns: repeat(${sideGrids}, 1fr)`;
+    totalGrid = sideGrids * sideGrids;
     for (let i = 0; i < totalGrid; i++){
         let grid = document.createElement('div');
         grid.classList.add('grid')
         pad.appendChild(grid)
     }
     let grids = document.querySelectorAll('.grid')
+    grids.forEach(grid => grid.addEventListener('mouseover', e => {
+        if (!mousehold) return;
+        e.target.style.cssText = 'background-color: black;';
+
+        }));
+}
+function isMouseDown(){
+    let status = false;
+    window.addEventListener('mousedown', e => {
+        
+        mousehold = true;
+    });
+    window.addEventListener('mouseup', e => mousehold = false)
+    console.group(status)
+    
 }
